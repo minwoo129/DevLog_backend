7a287a68c2913dbdfdc753b8ae8cd66fcbe38b69
minwoo129 <key0129mw@naver.com>
2023-07-29 16:47:42 +0900

    env, typeorm 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: Number(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      synchronize: true,
     }),
   ],
   controllers: [AppController],
