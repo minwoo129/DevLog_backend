0e06d2bc737f029b432aa14e87eda51a690c2569
minwoo129 <key0129mw@naver.com>
2023-08-05 17:40:21 +0900

    jwt 토큰 생성 로직 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,14 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
 import { LoginSuccessVO } from './vo/login-success.vo';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async login(email: string, password: string): Promise<LoginSuccessVO> {
     const user = await this.usersService.findByEmail(email);
@@ -17,13 +21,18 @@ export class AuthService {
     if (!pwdCheck) throw new UnauthorizedException();
 
     const { id, name, nickname, isAdmin } = user;
-    return {
+    let payload = {
       id,
       name,
       email,
       nickname,
       isAdmin,
-      authToken: '',
+    };
+    const authToken = await this.jwtService.signAsync(payload);
+
+    return {
+      ...payload,
+      authToken,
     };
   }
 }
