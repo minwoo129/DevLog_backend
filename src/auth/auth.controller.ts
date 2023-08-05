740ff66d006e34e96fab2515e6397ce40c833f0d
minwoo129 <key0129mw@naver.com>
2023-08-05 17:29:58 +0900

    로그인 api 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDTO } from './dto/login.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('/login')
+  login(@Body() loginDTO: LoginDTO) {
+    return this.authService.login(loginDTO.email, loginDTO.password);
+  }
+}
