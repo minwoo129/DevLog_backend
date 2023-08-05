b850210f66e6692e25ce60f7e0ae5137ef696829
minwoo129 <key0129mw@naver.com>
2023-08-05 17:24:12 +0900

    로그인 처리 service 함수 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
+import { LoginSuccessVO } from './vo/login-success.vo';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async login(email: string, password: string): Promise<LoginSuccessVO> {
+    const user = await this.usersService.findByEmail(email);
+
+    if (!user) throw new UnauthorizedException();
+
+    const pwdCheck = await bcrypt.compare(password, user.password);
+
+    if (!pwdCheck) throw new UnauthorizedException();
+
+    const { id, name, nickname, isAdmin } = user;
+    return {
+      id,
+      name,
+      email,
+      nickname,
+      isAdmin,
+      authToken: '',
+    };
+  }
+}
